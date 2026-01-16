d28632b8b2c36cd5c6c4e3069f80a1b04dc25796
Aditi Agrawal <agrawal.aditi@razorpay.com>
2026-01-16 20:31:10 +0530

    Local smtp server setup

--- consumer.go
+++ consumer.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/smtp"
 	"sync"
 )
 
@@ -9,7 +11,20 @@ func consumer(id int, ch chan Recipient, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for recipient := range ch {
-		fmt.Println(id, recipient)
+		smtpHost := "localhost"
+		smtpPort := "1025"
+
+		formattedMessage := fmt.Sprintf("To: %s\r\nSubject: Test Email\r\n\r\n%s\r\n", recipient.Email, "Testing Email campaign")
+		msg := []byte(formattedMessage)
+
+		fmt.Printf("Consumer %d sending email to %s\n", id, recipient.Email)
+
+		err := smtp.SendMail(smtpHost+":"+smtpPort, nil, "aditi@testing.com", []string{recipient.Email}, msg)
+		if err != nil {
+			log.Fatalf("Error sending email to %s: %v\n", recipient.Email, err)
+		}
+		
+		fmt.Printf("Consumer %d sent email to %s\n", id, recipient.Email)
 	}
 
 	//Waitgroup chalta kaise hai ?
