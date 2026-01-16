a25d10827a904a3f7dc63b2cdce03c5fa9184493
Aditi Agrawal <agrawal.aditi@razorpay.com>
2026-01-16 23:33:49 +0530

    Created the service layer, producer->channel->consumer

---
+++ internal/service/notification_service.go
@@ -0,0 +1,81 @@
+package service
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/aditiAgrawaldev/postera/internal/channels"
+	"github.com/aditiAgrawaldev/postera/internal/models"
+)
+
+type NotificationService struct {
+	channel channels.Channel
+}
+
+func NewNotificationService(channel channels.Channel) *NotificationService {
+	return &NotificationService{
+		channel: channel,
+	}
+}
+
+// Refactor karenge, same producer.go code
+func (n *NotificationService) loadRecipient(filePath string) ([]models.Recipient, error) {
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+
+	var recipients []models.Recipient
+	for _, record := range records[1:] {
+		recipients = append(recipients, models.Recipient{
+			Name:  record[0],
+			Email: record[1],
+		})
+	}
+
+	return recipients, nil
+}
+
+func (n *NotificationService) SendNotifications(notifications []models.Notification, consumerCount int) error {
+	notificationch := make(chan models.Notification)
+	var wg sync.WaitGroup
+
+	for i:=1; i<= consumerCount; i++ {
+		wg.Add(1)
+		go n.consumer(i,notificationch,&wg)
+	}
+
+	for _, notification := range notifications {
+		notificationch <- notification
+	}
+
+	close(notificationch)
+	wg.Wait()
+
+	return nil
+}
+
+func (n *NotificationService) consumer(id int, notificationch chan models.Notification, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for notification := range notificationch {
+		fmt.Printf("Worker %d sending notification to %s\n", id, notification.Recipient.Email)
+
+		err := n.channel.Send(notification)
+		if err != nil {
+			fmt.Printf("Worker %d failed to send notification to %s: %v\n", id, notification.Recipient.Email, err)
+		}else{
+			fmt.Printf("Worker %d sent notification to %s\n", id, notification.Recipient.Email)
+		}
+	}
+}
